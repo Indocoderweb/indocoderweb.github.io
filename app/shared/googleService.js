81b68a5d2c69fc5a49c8831901b477ee6963c369
TKJ Corp <17441900+SoftKopi@users.noreply.github.com>
2018-01-14 00:14:38 +0700

    Update googleService.js

--- app/shared/googleService.js
+++ app/shared/googleService.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  var CLIENT_ID = '608120956255-0me03edqv60mf1eilgdjoum9qcmv4deq.apps.googleusercontent.com';
+  var CLIENT_ID = '699652541625-h81tpn5vk43hdu76uso7o9bsc6gc7cf6.apps.googleusercontent.com';
   var SCOPES = [
         'https://www.googleapis.com/auth/drive',
         'https://www.googleapis.com/auth/drive.file',
